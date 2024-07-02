aba23f4a581315c7ea6fc988f13358b411eac675
Jakub Frąckiewicz <jakub.frackiewicz95@gmail.com>
2024-07-02 12:43:20 +0200

    testing changes

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Jakub Frąckiewicz!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
